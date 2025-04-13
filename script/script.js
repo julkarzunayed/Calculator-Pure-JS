55976b12b9ee2aac90e6c714a8a0558d34528ea5
julkarzunayed <zunayedjulkar@gmail.com>
2025-04-14 00:42:04 +0600

    added basic calculation functions

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ const data = fetch("https://raw.githubusercontent.com/julkarzunayed/Calculator-P
 const displayKeys = (keys) => {
     const keysContainer = document.getElementById("keys-container");
 
-    console.log(keys)
+    // console.log(keys)
     for (const key of keys) {
         const div = document.createElement("div")
         div.innerHTML = `
@@ -34,30 +34,115 @@ function liAdd() {
 //     // textAria.id.add("text-area-box")
 //     textContainer.appendChild(textAria)
 
-let textHolder = '123 11'
+let textHolder = ''
 
-function textareaUpdater () {
-    document.getElementById("text-area-box").value = textHolder
+function textareaUpdater() {
+    document.getElementById("text-box").innerText = textHolder
+    // document.getElementById("text-area-box").value = textHolder
 }
 
-function spaceChaker () {
-    const space = document.getElementById("text-area-box").value
+function spaceChecker() {
+    // const space = document.getElementById("text-area-box").value
     // if(space)
-    console.log(space.includes(" "))
+    // console.log(space.includes(" "))
 }
-spaceChaker()
+spaceChecker()
+
+function calculate() {
+    // if (!textHolder.includes("/") ||
+    //     !textHolder.includes("*")||
+    //     !textHolder.includes("-")||
+    //     !textHolder.includes("+")){
+    //         return textHolder;
+    // }
+    if (!/[/*-+-]/.test(textHolder)) {
+        return textHolder
+    }
+    let string2 = textHolder.split(/[/*-+-]/)
+    // let string2 = []
+    // if(textHolder)
+
+    let sum = 0;
+
+    if (string2.length >= 2) {
+        const stringF = textHolder.split("")
+        console.log(stringF)
+
+        for (let i = 0; i < stringF.length; i++) {
+            const first = Number(string2[0])
+            const second = Number(string2[1])
+            // if(){
+
+            // }
+            // console.log(stringF[i])
+            if (stringF[i] === "-") {
+                sum = first - second;
+                string2.shift()
+                string2.shift()
+                string2.unshift(sum)
+
+                // if (first < second) {
+                //     sum = second - first;
+                //     console.log("jksfjksdjf")
+                //     string2.shift()
+                //     string2.shift()
+                //     string2.unshift(-sum)
+                // }
+                // else {
+                //     sum = first - second;
+                //     string2.shift()
+                //     string2.shift()
+                //     string2.unshift(sum)
+                // }
+                console.log(string2, "kdjkfjdk", first, second)
+            }
+            else if (stringF[i] === "+") {
+                sum = first + second;
+                string2.shift()
+                string2.shift()
+                string2.unshift(sum)
+                console.log(string2)
+            }
+            if (stringF[i] === "/") {
+                sum = first / second;
+                string2.shift()
+                string2.shift()
+                string2.unshift(sum)
+                console.log(string2)
+            }
+            if (stringF[i] === "*") {
+                sum = first * second;
+                string2.shift()
+                string2.shift()
+                string2.unshift(sum)
+                console.log(string2)
+            }
+            
+
+        }
+        console.log(sum)
+        return sum;
+    }
+}
 
 function textContainerUpdate(text) {
-    const textarea = document.getElementById("text-area-box")
+    // const textarea = document.getElementById("text-area-box")
     // if()
     if (text === "C") {
         textHolder = ""
-        textarea.value = textHolder
+        textareaUpdater();
         return;
     }
     if (text === "=") {
-        textHolder = ""
-        textarea.value = textHolder
+        const result = calculate()
+        textHolder = result
+        textareaUpdater();
+        return;
+    }
+    if(text === "%"){
+        const result = calculate()
+        textHolder = `${result/100}`
+        textareaUpdater();
         return;
     }
     textHolder += text
@@ -69,7 +154,7 @@ function textContainerUpdate(text) {
     // console.log(jj)
 
 }
-function backSpace () {
+function backSpace() {
     const newTextHolder = textHolder.slice(0, -1);
     textHolder = newTextHolder;
     textareaUpdater()
@@ -80,9 +165,12 @@ const handleKeys = () => {
     document.getElementById("keys-container")
         .addEventListener("click", function (key) {
             // console.log(key.target.id)
-            if(key.target.id == 2 ){
+            if (key.target.id == 2) {
                 backSpace();
-                return
+                return;
+            }
+            if (key.target.id === "keys-container") {
+                return;
             }
             console.log(key.target.innerText)
 
@@ -93,7 +181,12 @@ const handleKeys = () => {
 
 handleKeys()
 
-let string = "123 45-123+1*11";
+let string = "12345/*-+6";
+
+console.log(!/[/*-+-]/.test(string))
+// console.log(string.match(""))
+// console.log(string.includes())
+
 
 // const newStr = string.slice(0, -1);
 // string = newStr
@@ -101,7 +194,69 @@ let string = "123 45-123+1*11";
 // console.log(string)
 
 let string2 = string.split(/[/*-+-]/)
-console.log(string2)
+let numbers = []
+for (const num of string2) {
+    numbers.unshift(Number(-num))
+}
+// console.log(string2)
+// console.log(numbers)
 
+
+// if (string2.length >= 2) {
+//     const stringF = string.split("")
+//     // const first = Number(string2[0])
+//     // const second = Number(string2[1])
+//     let sum = 0;
+//     console.log(stringF)
+
+//     for (let i = 0; i < stringF.length; i++) {
+//         const first = Number(string2[0])
+//         const second = Number(string2[1])
+//         // console.log(stringF[i])
+//         if (stringF[i] === "-") {
+//             if (first < second) {
+//                 sum = second - first;
+//                 console.log("jksfjksdjf")
+//                 string2.shift()
+//                 string2.shift()
+//                 string2.unshift(-sum)
+//             }
+//             else {
+//                 sum = first - second;
+//                 string2.shift()
+//                 string2.shift()
+//                 string2.unshift(sum)
+//             }
+//             // string2.shift()
+//             // string2.shift()
+//             // string2.unshift(sum)
+//             console.log(string2, "kdjkfjdk", first, second)
+//         }
+//         else if (stringF[i] === "+") {
+//             sum = first + second;
+//             string2.shift()
+//             string2.shift()
+//             string2.unshift(sum)
+//             console.log(string2)
+//         }
+//         if (stringF[i] === "/") {
+//             sum = first / second;
+//             string2.shift()
+//             string2.shift()
+//             string2.unshift(sum)
+//             console.log(string2)
+//         }
+//         if (stringF[i] === "*") {
+//             sum = first * second;
+//             string2.shift()
+//             string2.shift()
+//             string2.unshift(sum)
+//             console.log(string2)
+//         }
+//     }
+// }
+
+// console.log("test", 1  0)
+console.log("test", Number("1.252525"))
 
 
